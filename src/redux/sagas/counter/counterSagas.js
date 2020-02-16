83b9c94ff11ced7252215887fad296ca86581a07
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2020-02-16 14:11:24 +0100

    created unit tests for generators

--- src/redux/sagas/counter/counterSagas.js
+++ src/redux/sagas/counter/counterSagas.js
@@ -3,12 +3,12 @@ import { INCREMENT, DECREMENT, INCREMENT_ASYNC, DECREMENT_ASYNC} from '../../act
 
 const delay = (ms) => new Promise(res => setTimeout(res, ms))
 
-function* incrementAsync() {
+export function* incrementAsync() {
     yield call(delay, 1000)
     yield put({ type: INCREMENT })
 }
 
-function* decrementAsync() {
+export function* decrementAsync() {
     yield call(delay, 1000)
     yield put({ type: DECREMENT })
 }
--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,5 +1,5 @@
 import { all } from 'redux-saga/effects'
-import { watchIncrementAsync, watchDecrementAsync } from '../sagas/counter/sagas'
+import { watchIncrementAsync, watchDecrementAsync } from './counter/counterSagas'
 
 export default function* rootSaga() {
     yield all([
